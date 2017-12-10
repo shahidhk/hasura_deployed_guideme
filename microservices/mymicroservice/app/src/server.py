5e6c99c18384dcb5a8d14bcb78474b2db476c49b
Yash Sodha <yashsodha@gmail.com>
2017-12-10 17:40:58 +0530

    .

--- microservices/mymicroservice/app/src/server.py
+++ microservices/mymicroservice/app/src/server.py
@@ -3,19 +3,6 @@ from flask import jsonify, request
 import requests
 import json
 
-@app.route('/')
-def index():
-    return "Index Page"
-
-def func_for_face(f):
-    #print f
-    return "1"
-
-@app.route('/facerecognize', methods=['POST'])
-def index1():
-    #print request.json
-    imgURL = request.json["photourl"]
-    print("URL : " + imgURL)
-    p = FaceDetect(imgURL)
-    a = p.detect()
-    return jsonify({'id': a}), 201
+@app.route("/")
+def hello():
+    return json.dumps({"message":"Hello World!"})
